58d31af87fbf1225d9f02145c81e3d4dda147aa9
Phi Nguyen <phinq1910@gmail.com>
2020-08-28 10:50:14 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<script type="text/javascript" src="https://apis.google.com/js/api.js" gapi_processed="true" aria-hidden="true"></script>
+<script type="text/javascript" src="https://apis.google.com/js/client.js" gapi_processed="true" aria-hidden="true"></script>
+
+<script type="text/javascript" src="google-picker.js" gapi_processed="true" aria-hidden="true"></script>
+
+<button type="button" class="btn btn-secondary upload-google" onclick="inkgoGoogle.loadPicker()" data-toggle="tooltip" title="Upload files from Google Drive"><i class="mdi mdi-google-drive"></i> Google</button>
